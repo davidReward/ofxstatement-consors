83463f645147c0eb8bdde45f7d769e9602f7337f
Johannes Klug <jk@jklug.com>
2019-10-15 22:58:05 +0200

    Fine-tuning statement memo
    (and fixing an off-by-one bug)

--- src/ofxstatement/plugins/consors.py
+++ src/ofxstatement/plugins/consors.py
@@ -15,7 +15,7 @@ class ConsorsParser(CsvStatementParser):
     # 6 Verwendungszweck 
     # 7 Betrag in EUR
 
-    mappings = {"date": 1, "payee": 2, "memo": 6, "amount": 7}
+    mappings = {"date": 1, "amount": 7}
     date_format = "%d.%m.%Y"
 
     def split_records(self):
@@ -23,11 +23,8 @@ class ConsorsParser(CsvStatementParser):
 
     def parse_record(self, line):
 
-        if self.cur_record == 2:
+        if self.cur_record == 1:
             self.statement.currency = line[7].strip('Betrag in ')
-            return None
-
-        if self.cur_record <= 2:
             return None
 
         # Remove dots (German decimal point handling)
@@ -40,6 +37,8 @@ class ConsorsParser(CsvStatementParser):
 
         # fill statement line according to mappings
         sl = super(ConsorsParser, self).parse_record(line)
+
+        sl.memo = line[5] + "; " + line[6] + "; " + line[2]
         return sl
  
 
